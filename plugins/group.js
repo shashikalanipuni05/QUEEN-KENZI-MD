76e3775c4ef2c3f87409d1ce934327c84f6ae979
shashikalanipuni05 <160840691+shashikalanipuni05@users.noreply.github.com>
2024-08-28 18:20:57 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -48,3 +48,32 @@ reply('🦄 *GROUP IS OPEN MY BOT OWNER*')
 l(e)
 }
 })
+
+
+
+cmd({
+    pattern: "tagall",
+    react: "🔖",
+    desc: descg,
+    category: "group",
+    use: '.hidetag <hi>',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if (!isGroup) return reply(ONLGROUP)
+if (!isBotAdmins) return reply(botAdmin)
+if (!isAdmins) return reply(ADMIN)
+let teks = ` *ＧＲＯＵＰ  ＮＯＴＩＦＹ*
+                   
+*𝐌𝐄𝐒𝐒𝐀𝐆𝐄 : ${q ? q : 'blank'}*\n\n`
+for (let mem of participants) {
+teks += `🔵 @${mem.id.split('@')[0]}\n`
+     }
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id)})
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('')
+l(e)
+}
+})
