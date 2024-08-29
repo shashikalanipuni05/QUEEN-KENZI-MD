fcb3450c0587884fe119b5feefe62016bed422a8
shashikalanipuni05 <160840691+shashikalanipuni05@users.noreply.github.com>
2024-08-29 18:32:06 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -19,3 +19,27 @@ reply(`${e}`)
 })
 
 
+
+cmd({
+    pattern: "gemini",
+    desc: "Ai chat.",
+    react: "🧠",
+    category: "other",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let res = await fetchJson(`https://api.vihangayt.com/ai/gemini?q=${q}`)
+//----------------------------------
+let data = res.data
+
+await reply(data.response)
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+
