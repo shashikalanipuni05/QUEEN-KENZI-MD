05f412051603bb959705fd7a4d5c71188251d847
©ʍɾ ℂ𝕪𝕓𝕖𝕣 ʍɑղմӀ օƒƒíϲíɑӀ♠️ <157870475+manulofficial7@users.noreply.github.com>
2024-08-31 18:31:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -167,7 +167,15 @@ if(senderNumber.includes("94710432677")){
 if(isReact) return
 m.react("🦄")
 }        
-               
+//=======AutoVoice=========
+if (config.AUTO_VOICE === 'true') {
+const url = 'https://raw.githubusercontent.com/DarkYasiyaofc/VOICE/main/Voice-Raw/FROZEN-V2'
+let { data } = await axios.get(url)
+for (vr in data){
+if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
+ }}
+
+//==========================
 const events = require('./command')
 const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
 if (isCmd) {
