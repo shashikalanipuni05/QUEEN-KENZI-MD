e7a3fc2208102f448f9aef49ce1dc83f1b768a90
shashikalanipuni05 <160840691+shashikalanipuni05@users.noreply.github.com>
2024-09-05 20:29:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/02901a37c157fa1352aef.jpg",
-ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©DΛЯK ПΣӨП ᑕYBΣЯZ ``| 2024🤍*\n\n> *Uptime:*  ${runtime(process.uptime())}
-> *Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-> *HostName:* ${os.hostname()}
-> *Owner:* DanuXzz ジ\n\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀʀᴋɴᴇᴏɴᴄʏʙᴇʀꜱ ©",
+ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©DΛЯK ПΣӨП ᑕYBΣЯZ ``| 2024🤍*",
 AUTO_VOICE : process.env.AUTO_VOICE || "true",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
