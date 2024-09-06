358be9f83bb7301262ff851d68f12842cdec5972
shashikalanipuni05 <160840691+shashikalanipuni05@users.noreply.github.com>
2024-09-06 15:10:13 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -32,7 +32,7 @@ body: 'ᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ',
 mediaType: 1,
 sourceUrl: "https://github.com/Danupa0" ,
 thumbnailUrl: 'https://telegra.ph/file/08ce20a843b7650a296e7.jpg' ,
-renderLargerThumbnail: false,
+renderLargerThumbnail: true,
 showAdAttribution: false
 }
 }}, { quoted: mek})
