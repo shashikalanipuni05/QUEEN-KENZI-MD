cc77939f6bd634d69401618a59a469d1a143a47b
Danupa000 <dimuthumanjusri83@gmail.com>
2024-09-07 19:30:38 +0530

    Update Song+video.js

--- plugins/Song+video.js
+++ plugins/Song+video.js
@@ -162,6 +162,26 @@ arama.all.map((video) => {
 mesaj += ' *💃' + video.title + '*\n🔗 ' + video.url + '\n\n'
 });
 await conn.sendMessage(from , { text:  mesaj }, { quoted: mek } )
+    contextInfo: {
+    mentionedJid: [ '' ],
+    groupMentions: [],
+    forwardingScore: 999,
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: '120363192956026815@newsletter',
+      newsletterName: "Ｄａｒｋ Ｎｅｏｎ Ｃｙｂｅｒｚ 🅥",
+      serverMessageId: 999
+    },
+externalAdReply: { 
+title: '💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍',
+body: 'ᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ',
+mediaType: 1,
+sourceUrl: "https://github.com/Danupa0" ,
+thumbnailUrl: 'https://telegra.ph/file/02901a37c157fa1352aef.jpg' ,
+renderLargerThumbnail: true,
+showAdAttribution: true
+}
+}}, { quoted: mek})
 } catch (e) {
     l(e)
   reply('*Error !!*')
