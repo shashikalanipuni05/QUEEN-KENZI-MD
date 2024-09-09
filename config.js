c812b529fc1802434b4829c7449af6a1eb8f9358
shashikalanipuni05 <160840691+shashikalanipuni05@users.noreply.github.com>
2024-09-09 18:16:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,7 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/08ce20a843b7650a296e7.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©DΛЯK ПΣӨП ᑕYBΣЯZ ``| 2024🤍*",
 AUTO_VOICE : process.env.AUTO_VOICE || "true",
+MODE : process.env.MODE || "public",
+AUTO_BIO : process.env.AUTO_BIO || "true",    
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
