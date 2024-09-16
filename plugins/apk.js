2ff8662b707a141bd734e436dbf6ecadede893b6
DanuXzz ジ <160840691+shashikalanipuni05@users.noreply.github.com>
2024-09-16 08:40:34 +0530

    Create apk.js

---
+++ plugins/apk.js
@@ -0,0 +1,28 @@
+const apkdl = require('../lib/apkdl')
+const config = require('../config')
+const { cmd, commands } = require('../command')
+
+cmd({
+    pattern: "apk",
+    react: "📁",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+await conn.sendMessage(from, { react: { text: '📥', key: mek.key }})
+if(!q) return await conn.sendMessage(from , { text: '*Need apk link...*' }, { quoted: mek } ) 
+const data = await apkdl.download(q)
+let listdata = `*📚 Name :* ${data.name}
+*📦 Developer :* ${data.package}
+*⬆️ Last update :* ${data.lastup}
+*📥 Size :* ${data.size}`
+await conn.sendMessage(from, { image: { url: data.icon }, caption: listdata }, { quoted: mek })
+let sendapk = await conn.sendMessage(from , { document : { url : data.dllink  } , mimetype : 'application/vnd.android.package-archive' , fileName : data.name + '.' + 'apk',caption: '' } , { quoted: mek })
+await conn.sendMessage(from, { react: { text: '📁', key: sendapk.key }})
+await conn.sendMessage(from, { react: { text: '✔', key: mek.key }})
+} catch (e) {
+    reply('*ERROR !!*')
+  console.log(e)
+}
+})
